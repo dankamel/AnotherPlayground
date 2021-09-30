fc7c267e072bd8b1c002509e27aeb792f9bde513
dankamel <danielufkamel@gmail.com>
2021-09-30 20:31:26 +0100

    Initial Commit

---
+++ AnotherPlayground/AnotherPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherPlaygroundApp.swift
+//  AnotherPlayground
+//
+//  Created by Daniel Kamel on 30/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnotherPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
